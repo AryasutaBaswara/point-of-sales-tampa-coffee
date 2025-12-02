25529a2f617c36d573ec8eba7cc0317faf52eb42
AryasutaBaswara <baswarathunder22@gmail.com>
2025-12-02 21:52:22 +0700

    add login form layout

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import Login from "./_components/login";
+
+export const metadata = {
+  title: "Login | POS Cafe",
+  description: "Login to access the POS Cafe application.",
+};
+
+export default function LoginPage() {
+  return <Login />;
+}
