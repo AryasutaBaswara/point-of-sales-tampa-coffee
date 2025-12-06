37f2a77aa033fd3924fc56483e85d5bcdce09d25
AryasutaBaswara <baswarathunder22@gmail.com>
2025-12-06 20:20:28 +0700

    add state management profile using zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 // import Image from "next/image";
 import { DarkmodeToggle } from "../components/common/darkmode-toggle";
 import Link from "next/link";
+import { useAuthStore } from "@/stores/auth-store";
 
 export default function Home() {
+  const profile = useAuthStore((state) => state.profile);
   return (
     <div className="bg-muted flex flex-col justify-center items-center h-screen space-y-4">
-      <h1 className="text-4xl font-semibold">Welcome Aryasuta Baswara</h1>
+      <h1 className="text-4xl font-semibold">Welcome {profile.name}</h1>
       <Link href="/admin">
         <Button className="bg-teal-500 text-white">Access Dashboard</Button>
       </Link>
