4c4a4d75903ef32d30e2899d17e735e94068c737
AryasutaBaswara <baswarathunder22@gmail.com>
2026-01-02 11:20:40 +0700

    add form file handling for avatar

--- src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
+++ src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
@@ -20,12 +20,13 @@ import {
 } from "@/validations/auth-validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
-import { startTransition, useActionState, useEffect } from "react";
+import { startTransition, useActionState, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { create } from "zustand";
 import { createUser } from "../actions";
 import { toast } from "sonner";
 import FormSelect from "@/components/common/form-select";
+import FormImage from "@/components/common/form-image";
 
 export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
   const form = useForm<CreateUserForm>({
@@ -36,10 +37,14 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
   const [createUserState, createUserAction, isPendingCreateUser] =
     useActionState(createUser, INITIAL_STATE_CREATE_USER);
 
+  const [preview, setPreview] = useState<
+    { file: File; displayUrl: string } | undefined
+  >(undefined);
+
   const onSubmit = form.handleSubmit(async (data) => {
     const formData = new FormData();
     Object.entries(data).forEach(([key, value]) => {
-      formData.append(key, value);
+      formData.append(key, key === "avatar_url" ? preview!.file ?? "" : value);
     });
     startTransition(() => {
       createUserAction(formData);
@@ -56,6 +61,7 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
     if (createUserState?.status === "success") {
       toast.success("Create User Success");
       form.reset();
+      setPreview(undefined);
       document.querySelector<HTMLButtonElement>('[data-state="open"]')?.click();
       refetch();
     }
@@ -71,17 +77,26 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
         <form onSubmit={onSubmit} className="space-y-4">
           <FormInput
             form={form}
+            name="name"
+            label="Name"
+            placeholder="Insert your name"
+          />
+          <FormInput
+            form={form}
             name="email"
             label="Email"
             placeholder="Enter your email address"
             type="email"
           />
-          <FormInput
+
+          <FormImage
             form={form}
-            name="name"
-            label="Name"
-            placeholder="Insert your name"
+            name="avatar_url"
+            label="Avatar"
+            preview={preview}
+            setPreview={setPreview}
           />
+
           <FormSelect
             form={form}
             name="role"
