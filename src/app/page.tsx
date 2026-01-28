d525905e37b1632f8018aa9dba640065349bb142
AryasutaBaswara <baswarathunder22@gmail.com>
2026-01-28 20:58:35 +0700

    refactor access dashboard redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,11 @@ export default function Home() {
     <div className="bg-muted flex flex-col justify-center items-center h-screen space-y-4">
       <h1 className="text-4xl font-semibold">Welcome {profile.name}</h1>
       <Link
-        href={profile.role !== "kitchen" || "cashier" ? "/order" : "/admin"}
+        href={
+          profile.role === "kitchen" || profile.role === "cashier"
+            ? "/order"
+            : "/admin"
+        }
       >
         <Button className="bg-teal-500 hover:bg-teal-600 text-white">
           Access Dashboard
